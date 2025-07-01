6be2d45949626975d7595c3224553b9929190f10
DeveloperAvanishKumar <ak.jasrana97@gmail.com>
2025-07-02 00:46:44 +0530

    update

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
-const app = express(); // ✅ app first
 
+const app = express();
 app.use(express.json());
 
-// ✅ Serve static uploads folder
-app.use('/uploads', express.static('uploads'));
-
-app.use(express.urlencoded({ extended: true }))
-    // ✅ Mount user routes
-const userRoutes = require('./App/routes/mobile/userRoutes');
-const categoryRoutes = require('./App/routes/mobile/categoryRoutes');
-app.use('/web/api', categoryRoutes);
-app.use('/web/api', userRoutes);
-
-// ✅ MongoDB connection and server start
-
-// Optional: Turn off strict query warnings
 mongoose.set('strictQuery', false);
 
 mongoose.connect(process.env.DBURL)
     .then(() => {
-        console.log(" Database Connected");
+        console.log("✅ Database Connected");
         app.listen(process.env.PORT || 3000, () => {
-            console.log(`Server running at http://localhost:${process.env.PORT || 3000}`);
+            console.log(` Server running at http://localhost:${process.env.PORT || 3000}`);
         });
     })
     .catch((err) => {
